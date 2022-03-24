a0a3a351202d40fe9f82d1cc584a5cc5f738c7dc
weinvest <asd1831031@163.com>
2022-03-24 16:24:53 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./configure --enable-ssl  --with-precision-digits=8 --with-thread=stdthread --with-precision=double --with-maxmsglen=65536 
+#vi utests/Makefile: message_test$(EXEEXT): $(message_test_DEPENDENCIES) $(message_test_OBJECTS)
+make -j4 install
