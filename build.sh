b5404f417356839449fc03ad6c565a2ac0b1766b
weinvest <asd1831031@163.com>
2022-03-24 16:39:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./configure --enable-ssl  --with-precision-digits=8 --with-thread=stdthread --with-precision=double --with-maxmsglen=65536 
+./configure --enable-ssl  --with-precision-digits=8 --with-thread=stdthread --with-precision=double --with-maxmsglen=32768 
 #vi utests/Makefile: message_test$(EXEEXT): $(message_test_DEPENDENCIES) $(message_test_OBJECTS)
 make -j4 install
